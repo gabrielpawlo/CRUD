29858649c114ed90293926e1c268807693e31ccc
Gabriel Pawlowski <gabrielgpawlowski@gmail.com>
2025-10-05 20:31:24 -0300

    projeto CRUD inciado

---
+++ CrudProjeto/Models/Usuarios.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudProjeto.Models;
+
+public class Usuarios
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
